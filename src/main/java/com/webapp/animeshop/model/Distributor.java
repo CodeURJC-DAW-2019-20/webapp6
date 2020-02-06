703733a4bfed9faca3e8cc2529a015ed7ec5e78c
CarlosFco <35729190+CarlosFco@users.noreply.github.com>
2020-02-06 12:34:54 +0100

    Distributor and Franchise enums created

---
+++ src/main/java/com/webapp/animeshop/model/Distributor.java
@@ -0,0 +1,13 @@
+package com.webapp.animeshop.model;
+
+public enum Distributor {
+	Tsume,
+	Funko_Pop,
+	Figurama,
+	Firts4Figures,
+	Banpresto,
+	Portrait_of_Pirates,
+	Megahouse,
+	Kotobukiya,
+	Bandai_namco_Arts
+}
---
+++ src/main/java/com/webapp/animeshop/model/Franchise.java
@@ -0,0 +1,15 @@
+package com.webapp.animeshop.model;
+
+public enum Franchise {
+	Naruto,
+	One_Piece,
+	Attack_on_Titan,
+	My_Hero_Academia,
+	Bleach,
+	Dragon_Ball,
+	Fullmetal_Alchemist,
+	Bloodborne,
+	Black_Clover,
+	Demon_Slayer,
+	Fairy_Tail
+}
