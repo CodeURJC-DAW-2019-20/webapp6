06200b00cec9f89c155cc310fd22d32ef68bc640
David <David.a.almendra@hotmail.com>
2020-02-09 18:07:26 +0100

    Delete and add product functionality plus css styles

--- src/main/java/com/webapp/animeshop/controller/ProductController.java
+++ src/main/java/com/webapp/animeshop/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.webapp.animeshop.model.Product;
 import com.webapp.animeshop.repositories.ProductRepository;
@@ -33,11 +34,22 @@ public class ProductController {
 		return "/singleProduct";
 	}
 	
-	@RequestMapping("/category/addProduct")
-	public String addProduct(Model model, @RequestParam String name) {
-		Product product = new Product();
+	@RequestMapping("/addProduct")
+	public String addProduct(Model model, @RequestParam String name, @RequestParam String franchise, @RequestParam String price,
+							 @RequestParam String description, @RequestParam int width, @RequestParam int height,
+							 @RequestParam int weight, @RequestParam String distributor, @RequestParam String reference) {
+		
+		Product product = new Product(name,franchise,Double.parseDouble(price),description,width,height,weight,distributor,reference);
 		this.productService.addProduct(product);
 		return this.showProducts(model);
 	}
+	
+	@RequestMapping("/deleteProduct/{id}")
+	public String deleteProduct(Model model, @PathVariable long id) {
+		productService.deleteProduct(id);
+		return this.showProducts(model);
+	}
+	
+	
 	
 }
