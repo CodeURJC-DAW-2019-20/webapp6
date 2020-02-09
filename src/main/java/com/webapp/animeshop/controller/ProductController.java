9c7f14e47ebd8ea551216dcdeaf814f658e92164
David <David.a.almendra@hotmail.com>
2020-02-09 21:30:40 +0100

    Added deleted method when merge to master

--- src/main/java/com/webapp/animeshop/controller/ProductController.java
+++ src/main/java/com/webapp/animeshop/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.webapp.animeshop.model.Product;
 import com.webapp.animeshop.repositories.ProductRepository;
@@ -43,4 +44,10 @@ public class ProductController {
 		return this.showProducts(model);
 	}
 	
+	@RequestMapping("/deleteProduct/{id}")
+	public String deleteProduct(Model model, @PathVariable long id) {
+		productService.deleteProduct(id);
+		return this.showProducts(model);
+	}
 }
+
