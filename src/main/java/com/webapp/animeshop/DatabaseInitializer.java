1636ccfb03b25dfbfc39664b82adf6cacb789c52
David <David.a.almendra@hotmail.com>
2020-02-13 18:55:36 +0100

    Product filters and fixes

--- src/main/java/com/webapp/animeshop/DatabaseInitializer.java
+++ src/main/java/com/webapp/animeshop/DatabaseInitializer.java
@@ -40,19 +40,23 @@ public class DatabaseInitializer {
 		this.userRepository.save(user);
 		this.userRepository.save(admin);
 		
-		Product product = new Product("Funko Pop! Levi Ackerman","Attack on Titans",14.49,"WOW",5,9,200,"Funko Pop!","TM0ZZH441XA", "/img/product/levimin.png", "/img/product/levi2.jpg");
-		Product product2 = new Product("One Piece - Charlotte Katakuri (Figuarts ZERO)","One Piece",69.99,"WOW",9,16,500,"Figuarts ZERO","TM0ZZH441XB", "/img/product/katakuri.png", "/img/product/katakurifull.jpg");
-		Product product3 = new Product("Pack Naruto y Sasuke Relation (Figuarts ZERO Ex)","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg");
-		this.productService.addProduct(product);
-		this.productService.addProduct(product2);
-		this.productService.addProduct(product3);
+		this.productService.addProduct(new Product("Funko Pop! Levi Ackerman","Attack on Titans",14.49,"WOW",5,9,200,"Funko Pop!","TM0ZZH441XA", "/img/product/levimin.png", "/img/product/levi2.jpg"));
+		this.productService.addProduct(new Product("One Piece - Charlotte Katakuri (Figuarts ZERO)","One Piece",69.99,"WOW",9,16,500,"Figuarts ZERO","TM0ZZH441XB", "/img/product/katakuri.png", "/img/product/katakurifull.jpg"));
+		this.productService.addProduct(new Product("Pack Naruto y Sasuke Relation (Figuarts ZERO Ex)","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº4","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº5","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº6","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº7","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº8","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº9","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº10","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº11","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
+		this.productService.addProduct(new Product("Producto Nº12","Naruto",141.25,"WOW",38,19,1200,"Figuarts ZERO","TM0ZZH441XC", "/img/product/narutosasuke.png", "/img/product/narutosasukefull.jpg"));
 		
-		Blog blog = new Blog("Administrador", "Nuevo Funko Pop! de Levi Ackerman - Attack on Titans", "No pierdas la oportunidad de hacerte con esta nueva pieza por parte de Funko Pop! de uno de los personajes más icónicos de Attack on Titans.", "img/blog/levi-blog.png");
-		Blog blog2 = new Blog("Administrador", "Nueva figura Naruto Kyuubi mode por parte de Figuarts ZERO", "Y es que otra vez lo han vuelto a hacer.. Figuarts ZERO nos sorprende con esta magnifica figura de Naruto en modo Kyuubi para los más amantes de la serie.", "img/blog/naruto-blog.png");
-		Blog blog3 = new Blog("Administrador", "INCREÍBLE busto M.U.B Predator by Tsume", "Precio no apto para cardiacos. Eso sí, es una obra de arte de los piés a la cabeza.", "img/blog/predator-min.png");
-		this.blogService.addBlog(blog);
-		this.blogService.addBlog(blog2);
-		this.blogService.addBlog(blog3);
+		
+		this.blogService.addBlog(new Blog("Administrador", "Nuevo Funko Pop! de Levi Ackerman - Attack on Titans", "No pierdas la oportunidad de hacerte con esta nueva pieza por parte de Funko Pop! de uno de los personajes más icónicos de Attack on Titans.", "img/blog/levi-blog.png"));
+		this.blogService.addBlog(new Blog("Administrador", "Nueva figura Naruto Kyuubi mode por parte de Figuarts ZERO", "Y es que otra vez lo han vuelto a hacer.. Figuarts ZERO nos sorprende con esta magnifica figura de Naruto en modo Kyuubi para los más amantes de la serie.", "img/blog/naruto-blog.png"));
+		this.blogService.addBlog(new Blog("Administrador", "INCREÍBLE busto M.U.B Predator by Tsume", "Precio no apto para cardiacos. Eso sí, es una obra de arte de los piés a la cabeza.", "img/blog/predator-min.png"));
 		
 	}
 }
--- src/main/java/com/webapp/animeshop/controller/CustomWebMvcConfigurerAdapter.java
+++ src/main/java/com/webapp/animeshop/controller/CustomWebMvcConfigurerAdapter.java
@@ -1,6 +1,7 @@
 package com.webapp.animeshop.controller;
 
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,4 +12,9 @@ public class CustomWebMvcConfigurerAdapter implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/new/").setViewName("forward:/new/index.html");
 	}
+	
+	/*@Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+    }*/
 }
