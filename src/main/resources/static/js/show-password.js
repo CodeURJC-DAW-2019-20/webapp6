d8447555e57994066eb25911147856620dbd0e75
David <David.a.almendra@hotmail.com>
2020-02-24 22:29:46 +0100

    Show/hide password

--- src/main/resources/static/js/show-password.js
+++ src/main/resources/static/js/show-password.js
@@ -1,8 +1,11 @@
 function showPass() {
-  var x = document.getElementById("password-edit");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+$(".toggle-password").click(function() {
+	$(this).toggleClass("fa-eye fa-eye-slash");
+	var x = document.getElementById("password-edit");
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+});
 }
