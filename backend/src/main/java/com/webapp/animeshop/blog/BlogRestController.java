e7d806e8eff5261994090d34742b4fd0d3d53ac4
Enrique <kikemarquezjimenez@gmail.com>
2020-03-05 12:07:28 +0100

    Search method in BlogRestController

--- backend/src/main/java/com/webapp/animeshop/blog/BlogRestController.java
+++ backend/src/main/java/com/webapp/animeshop/blog/BlogRestController.java
@@ -1,14 +1,17 @@
 package com.webapp.animeshop.blog;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +60,14 @@ public class BlogRestController {
 		blog.setProduct(productService.getProduct(Long.parseLong(idproduct)));
 		this.blogService.addBlog(blog);
 		return blogService.addBlog(blog);
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<Collection<Blog>> search(@RequestBody String key) {
+		List<Blog> blogs = this.blogService.search(key);
+		if(blogs.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<Blog>>(blogs,HttpStatus.OK);
 	}
 }
