dade464dd3d7164904b8ef7edd7b5613efe95d50
ram2701 <rbarrabesp@gmail.com>
2020-03-05 18:28:16 +0100

    Solved create_image.bat bug

--- docker/create_image.ps1
+++ docker/create_image.ps1
@@ -1,10 +1,10 @@
 cd ../backend
-docker run --rm --name maven -v ${PWD}:/usr/src -w /usr/src maven:3.6-jdk-8 call mvn clean install
+call mvn clean install
 
 :: Copy jar file to docker folder
-
-copy target\itinerary-0.0.1-SNAPSHOT.jar ..\docker\app
-cd ../docker
+cd target
+copy animeshop-0.0.1-SNAPSHOT.jar ..\..\docker\app
+cd ../../docker
 
 :: Create and push image
 docker build -t ram2701/animeshop .
