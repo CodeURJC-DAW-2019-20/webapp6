ac847aefb67b27aa278897d6ad1473ca9f3fbb58
CarlosFco <35729190+CarlosFco@users.noreply.github.com>
2020-04-05 19:51:18 +0200

    Updated all blogs page

--- frontend/src/app/blog/blog.component.ts
+++ frontend/src/app/blog/blog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Blog} from './blog.model';
+import { BlogService } from './blog.service';
+
 
 @Component({
   selector: 'app-blog',
@@ -7,7 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  blog: Blog[] = [];
+
+  constructor(private router: Router, activatedRoute: ActivatedRoute, private service: BlogService) {
+    const id = activatedRoute.snapshot.params.id;
+    service.getAllBlogs().subscribe((blogs => this.blog = blogs), (error) => console.error(error));
+   }
 
   ngOnInit(): void {
   }
