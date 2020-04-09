4a21f8530f75bcda4d5b1793b84ab24d83fc95c8
CarlosFco <35729190+CarlosFco@users.noreply.github.com>
2020-04-09 19:20:24 +0200

    right list of blog entries in singleblog

--- frontend/src/app/blog/singleBlog.component.ts
+++ frontend/src/app/blog/singleBlog.component.ts
@@ -4,6 +4,7 @@ import { Blog } from './blog.model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from '../auth/login.service';
 import { ProductService } from '../product/product.service';
+import { Observable, interval, Subscription } from 'rxjs';
 
 @Component ( {
     selector: 'app-blog',
@@ -11,34 +12,39 @@ import { ProductService } from '../product/product.service';
     styleUrls: ['./blog.component.css']
 } )
 
-export class SingleBlogComponent {
+export class SingleBlogComponent implements OnInit{
 
     blog: Blog;
-    //blogs: Blog[] = []; blogs barra derecha
+    blogs: Blog[] = []; //blogs barra derecha
+    updateSubscription: Subscription;
 
     constructor(private router: Router, activatedRoute: ActivatedRoute, public service: BlogService, public loginService: LoginService) {
         const id = activatedRoute.snapshot.params.id;
         service.getBlogById(id).subscribe((blog => this.blog = blog),(error) => console.error(error));
         console.log(loginService.user.name)
 
-        //blogs barra derecha
-        //service.getAllBlogs().subscribe(
-        //    (blogs => this.blogs = blogs),
-        //    (error) => console.error(error));
-
-        //console.log(this.blogs.length.toString())
+    }
+    ngOnInit(): void {
+        this.service.getAllBlogs().subscribe(
+            (blogs => this.blogs = blogs),
+            (error) => console.error(error));
     }
 
     deleteBlog() {
         this.service.deleteBlog(this.blog).subscribe((_) => this.router.navigate(['/blog']), (error) => console.error(error));
     }
 
-    //ngOnInit(): void {
-        //blogs barra derecha
-        /*this.service.getAllBlogs().subscribe(
-            (blogs => this.blogs = blogs),
-            (error) => console.error(error));
-            console.log(this.blogs.length.toString())*/
-    //}
+    search(key: string) {
+        this.service.getBlogsByKey(key).subscribe(
+          blog => this.blogs = blog,
+          error => console.log(error)
+        );
+      }
+
+ /*public navToBlog(blogId: number) {
+        this.router.navigate(['/singleBlog', blogId]);
+        
+      } */
+
 
 }
