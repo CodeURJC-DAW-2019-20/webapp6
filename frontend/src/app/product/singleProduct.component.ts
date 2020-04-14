99a561b065d7ecda59f98f4a5e6e3115f1046d19
David <David.a.almendra@hotmail.com>
2020-04-14 22:47:01 +0200

    Some single product styles and angular materials tags. Add product in singleproduct page working.

--- frontend/src/app/product/singleProduct.component.ts
+++ frontend/src/app/product/singleProduct.component.ts
@@ -3,6 +3,9 @@ import { ProductService} from './product.service';
 import { Product} from './product.model';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from '../auth/login.service';
+import { OrderService } from '../order/order.service';
+import { Order } from '../order/order.model';
+import { ProductAmount} from './productamount.model';
 
 @Component({
   selector: 'app-product',
@@ -13,15 +16,34 @@ import { LoginService } from '../auth/login.service';
 export class SingleProductComponent {
 
   product: Product;
+  pAmount: ProductAmount;
+  order: Order;
+  qty: number;
 
-  constructor(private router: Router, activatedRoute: ActivatedRoute, public service: ProductService, public loginService: LoginService) {
+  constructor(private router: Router, activatedRoute: ActivatedRoute, public service: ProductService, public loginService: LoginService, private orderService: OrderService) {
     const id = activatedRoute.snapshot.params.id;
     service.getProductById(id).subscribe((product => this.product = product), (error) => console.error(error));
   }
 
+  ngOnInit() {
+    this.qty = 0;
+    this.orderService.getCurrentOrder().subscribe(
+      order => this.order = order,
+      error => console.log(error)
+    );
+  }
+
   deleteProduct() {
     this.service.deleteProduct(this.product).subscribe((_) => this.router.navigate(['/product']), (error) => console.error(error));
+  }
 
+  addProductToOrder(productAux: Product, qt: number){
+    let productAmount = { product: productAux, amount: qt }
+    this.pAmount = productAmount;
+    this.orderService.addProduct(this.pAmount, this.order.id ).subscribe(
+      order => { this.order = order;
+                  this.router.navigate(['/order']);
+      },error => console.log(error));
   }
 
 }
